389622a00a4d1a5b27ea25e5b166657a0f23bba9
Andi <getdata@duck.com>
2023-07-23 18:52:31 +0700

    rename.sh

--- rename.sh
+++ rename.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-for file in  $(ls | grep "json" | grep -Ev "[0-9]{4}-[0-9]{2}-[0-9]{2}");
-do
-  mv $file ${file%.*}_$(date +"%FT%H:%M%z").json;
+for file in  $(ls | grep -E "(csv|json)" | grep -Ev "[0-9]{4}-[0-9]{2}-[0-9]{2}"); do
+  mv $file ${file%.*}_$(date +"%FT%H:%M%z").${file##*.}
 done;
